49d1e25b942445e0a6e2b3cf70974651ffc8a2c3
Franz Herm <f.herm@ostfalia.de>
2023-11-08 09:40:53 +0000

    Ran Experiements and appended csv to gitignore

--- src/main.py
+++ src/main.py
@@ -23,30 +23,22 @@ def increment_indices(current_indices, max_indices):
         else:
             break
     return current_indices
+
 ####################################################
 ############## Program configuration ###############
-tsp_config_real = {
+tsp_config = {
     "distance_matrix_names": ["../xml/burma14.xml", "../xml/brazil58.xml"],
     "crossover_functions": [crossover.PmxCrossover, crossover.OrderCrossover, crossover.CrossoverWithFix],
     "mutation_functions":  [mutation.SwapMutation, mutation.InversionMutation, mutation.InsertMutation],
     "replacement_functions": [replacement.ReplaceWeakest, replacement.ReplaceFirstWeakest],
     "mutation_rates": [0.15, 0.5],
     "crossover_rates": [0.3, 0.8],
-    "tournament_rates": [0.05, 0.10, 0.5],
+    "tournament_rates": [0.05, 0.15, 0.5],
     "population_sizes": [25,100,500]
 }
-tsp_config = {
-    "distance_matrix_names": ["../xml/burma14.xml"],
-    "crossover_functions": [crossover.PmxCrossover],
-    "mutation_functions":  [mutation.SwapMutation],
-    "replacement_functions": [replacement.ReplaceWeakest],
-    "mutation_rates": [0.15],
-    "crossover_rates": [0.3],
-    "tournament_rates": [0.05],
-    "population_sizes": [25,100,500]
-}
-runs_per_experiment = 1
+runs_per_experiment = 10
 iterations_per_run = 10000
+
 ####################################################
 ########### Compute auxiliary parameters ###########
 distance_matrices = [get_distance_matrix_from_city_xml(x) for x in tsp_config["distance_matrix_names"]]
@@ -57,11 +49,13 @@ max_parameter_indices = np.array([len(x)-1 for x in tsp_config.values()], dtype=
 num_of_experiments = np.cumprod(max_parameter_indices + 1)[-1]
 column_headers = EvolutionaryAlgorithm.population_metrics_headers
 column_header_length = len(column_headers)
+
 ####################################################
-################### Data storage ###################
+############### Prepare data storage ###############
 data_overview = np.empty(shape=(num_of_experiments,column_header_length))
 data_detailed = np.empty(shape=(num_of_experiments,iterations_per_run, column_header_length))
 data_parameter_values = []
+
 ####################################################
 ################ Program execution #################
 for experiment_index in tqdm(range(num_of_experiments), desc="Experiments", position=0, leave=False):
@@ -79,7 +73,7 @@ for experiment_index in tqdm(range(num_of_experiments), desc="Experiments", posi
     # deduce additional parameters from config variables
     chromosome_size = distance_matrix.shape[0]-1
     crossover_function = CrossoverFunctionClass(chromosome_size)
-    mutation_function = MutationFunctionClass(chromosome_size)
+    mutation_function = MutationFunctionClass()
     replacement_function = ReplacementFunctionClass()
     fitness_function = FitnessFunction(distance_matrix)
     selection_function = TournamentSelection(max(1,int(tournament_rate * pop_size)))
@@ -93,8 +87,6 @@ for experiment_index in tqdm(range(num_of_experiments), desc="Experiments", posi
 
     #increment indices
     current_parameter_indices = increment_indices(current_parameter_indices, max_parameter_indices)
-
-    #fetch experiment data
     #average data from the individual runs
     data_detailed[experiment_index] = np.average(experiment_data,axis=0)
     #configuration of experiment
@@ -108,6 +100,7 @@ for experiment_index in tqdm(range(num_of_experiments), desc="Experiments", posi
 data_overview[:,0] = data_detailed[:,-1,0]
 data_overview[:,1] = data_detailed[:,-1,1]
 data_overview[:,2] = data_detailed[:,-1,2]
+
 ####################################################
 ##################### Save Data ####################
 if not os.path.exists("../csv"): #create csv directory
@@ -127,5 +120,8 @@ overview_df.to_csv("../csv/overview_df.csv")
 detailed_df.to_csv("../csv/detailed_df.csv")
 
 #display dataframe
-pd.set_option('display.max_columns', None); 
-print(detailed_df)
+#pd.set_option("display.max_columns",10000)
+#print(overview_df)
+#pd.set_option('display.max_columns', None)
+#detailed_df.unstack(level="Experiment")["AvgCost"].plot.line()
+#plt.show()
